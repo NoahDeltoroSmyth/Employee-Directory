08cef88b2fc4fbb4ba3be9fd09fce0daa4e76e70
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-24 10:07:17 -0800

    services functions in place

---
+++ src/services/client.js
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
+export const client = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABSE_KEY
+);
+
+export const parseData = ({ data, error }) => {
+  if (error) throw error;
+  return data;
+};
