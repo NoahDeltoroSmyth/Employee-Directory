f4989193c66cd22d1a8b382c641d4d938b437b1e
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-24 09:41:43 -0800

    router in place

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,31 @@
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/sign-in">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/create-profile">
+            <CreateProfile />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/edit-profile">
+            <EditProfile />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
